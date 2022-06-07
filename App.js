87eff376430795ff1a0105c44b29a9911f9ccf01
Aluno Laboratório IFPR <aluno@aluno.com>
2022-06-06 21:00:04 -0300

    2º formar do express

--- App.js
+++ App.js
@@ -2,27 +2,23 @@
 
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
 
-class App{
-    static async init(){
-        let app = new express();
-        app.use(cors());
-        app.use(express.json);
+let app = express();
 
-        app.get("/endpoint", (req, res) => {
-            console.log("Cheguei")
-            res.json({
-                name: "ifio-api-2022",
-                version: "1.0.0",
-                description: "Projeto para fins didáticos",
-                author: "Marcelo F. Terenciani"
-            })
-        })
+app.use(cors());
+app.use(express.json);
 
-        app.listen(3000, () => {
-            console.log("Startou")
-        })
-    }
-}
-App.init();
+app.get("/", (req, res) => {
+    console.log("Cheguei")
+    res.json({
+        name: "ifio-api-2022",
+        version: "1.0.0",
+        description: "Projeto para fins didáticos",
+        author: "Marcelo F. Terenciani"
+    })
+})
+
+const server = app.listen(3000, () => {
+    console.log("Startou")
+})
+module.exports = {app, server}
