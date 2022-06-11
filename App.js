e5779f90048ad801320de6bf77dc97ba314c879f
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2022-06-11 17:22:23 -0300

    Coreção do erro produzido no laboratório

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
-"use strict"
-
 const express = require("express");
 const cors = require("cors");
+const porta = process.env.PORT || 3000;
 
-let app = express();
+class App {
+    static async init() {
+        let app = new express();
+        app.use(cors());
+        app.use(express.json());
 
-app.use(cors());
-app.use(express.json);
+        app.get("/", (req, res) => {
+            res.json({
+                name: "ifio-api-2022",
+                version: "1.0.0",
+                description: "Projeto para fins didáticos",
+                author: "Marcelo F. Terenciani"
+            })
+        })
 
-app.get("/", (req, res) => {
-    console.log("Cheguei")
-    res.json({
-        name: "ifio-api-2022",
-        version: "1.0.0",
-        description: "Projeto para fins didáticos",
-        author: "Marcelo F. Terenciani"
-    })
-})
-
-const server = app.listen(3000, () => {
-    console.log("Startou")
-})
-module.exports = {app, server}
+        app.listen(porta, () => {
+            console.log(`Servidor inicializado na porta: ${porta}`)
+        })
+    }
+}
+App.init();
