36d116d72c5651d835f6f7e8c9fd2639eeee973f
= <=>
2022-07-04 20:31:56 -0300

    Demonstração das rotas - service não funcional

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,10 +4,46 @@ const UserService = require("../services/UserService")
 
 module.exports = class UserController {
     static async getUsers(req, res) {
-        res.status(200).send(UserService.getUsers())
-    }
+        try {
+            res.status(200).send(await UserService.getUsers());
+        } catch (e) {
+            res.status(500).send(e.message);
+            console.error('UserController.getUsers ' + e.message);
+        }
+    } // getUsers()
+    static async findById(req, res) {
+        try {
+            res.status(200).send(await UserService.findById(req.params.id_user));
+        } catch (e) {
+            res.status(500).send(e.message);
+            console.error('UserController.getUserData ' + e.message);
+        }
+    } // findById()
+    static async delete(req, res) {
+        try {
+            if (!req.body.id_user)
+                throw new Error('A identificação do usuário deve ser informada.');
 
+            res.status(200).send(await UserService.delete(req.body));
+        } catch (e) {
+            res.status(500).send(e.message);
+            console.error('UserController.delete ' + e.message);
+        }
+    } // delete()
     static async save(req, res) {
-        res.status(200).send(UserService.save(req.body))
-    }
+        try {
+            res.status(200).send(await UserService.save(req.body));
+        } catch (e) {
+            res.status(500).send(e.message);
+            console.error('UserController.save ' + e.message);
+        }
+    } // save()
+    static async update(req, res) {
+        try {
+            res.status(200).send(await UserService.update(req.body));
+        } catch (e) {
+            res.status(500).send(e.message);
+            console.error('UserController.update ' + e.message);
+        }
+    } // update()
 }
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,7 +3,12 @@ const UserController = require("../controllers/UserController")
 
 module.exports = class UserRoute {
     constructor(app) {
-        app.route("/user").get(UserController.getUsers);
-        app.route("/user").post(UserController.save);
+        app.route("/user")
+            .get(UserController.getUsers)
+            .post(UserController.save)
+            .put(UserController.update)
+            .delete(UserController.delete)
+        
+        app.route('/users/:id_user').get(UserController.findById);
     }
 }
